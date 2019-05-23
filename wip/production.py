95e7b108bac74b0067ebeb7723b4bc0af9d40c2a
Mitchell Burton <mitch@mitchellburton.ca>
2019-05-23 14:22:21 -0700

    update dependencies and deployment settings

--- wip/production.py
+++ wip/production.py
@@ -3,14 +3,26 @@ Django settings for wip production.
 """
 import os
 
-import dj_database_url
+import django_heroku
 
 from .settings import *
 
+# Miscellaneous deploy settings
+DEBUG = False
+SECURE_HSTS_SECONDS = 31536000  # One year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+X_FRAME_OPTIONS = 'DENY'
+
+ALLOWED_HOSTS = ['wiki.mitchellburton.ca']
+
+# Use env secret key
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', SECRET_KEY)
 
-INSTALLED_APPS += ['django.contrib.postgres']
-
-# Update database configuration with $DATABASE_URL
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# Activate Django-Heroku
+django_heroku.settings(locals())
